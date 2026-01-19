3da2dd66489b00ecb26766855dc5f3e69b35d674
hcmus-phat <23120317@student.hcmus.edu.vn>
2026-01-20 00:31:49 +0700

    fix: base api

--- frontend-admin/src/config/api.js
+++ frontend-admin/src/config/api.js
@@ -1,8 +1,10 @@
 // src/config/api.js (hoặc apiConfig.js)
 import axios from "axios";
 
-const API_BASE_URL =
+const BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+
+const API_BASE_URL = `${BASE_URL}/api`;
 
 const adminApi = axios.create({
   baseURL: `${API_BASE_URL}/admin`,
